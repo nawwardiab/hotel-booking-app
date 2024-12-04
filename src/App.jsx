4728b2933afb65f89f57d3d2878fac68f2375aa6
Irina Holler <irinaniko@hotmail.com>
2024-12-04 15:48:01 +0100

    fix the App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,13 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import RoomSearchPage from './pages/design/RoomSearchPage';
-import RoomDetailPage from './pages/design/RoomDetailPage';
-import BookingPage from './pages/design/BookingPage';
-import PaymentPage from './pages/design/PaymentPage';
-import CreditCardForm from './components/payment/CreditCardForm';
 import { BookingProvider } from './context/BookingContext';
 import { SearchProvider } from './context/SearchContext';
+import AppRoutes from './routes/AppRoutes';
 
 function App() {
   return (
     <SearchProvider>
       <BookingProvider>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<RoomSearchPage />} />
-            <Route path="/search" element={<RoomSearchPage />} />
-            <Route path="/room/:roomId" element={<RoomDetailPage />} />
-            <Route path="/booking" element={<BookingPage />} />
-            <Route path="/payment" element={<CreditCardForm />} />
-            <Route path="*" element={<RoomSearchPage />} />
-          </Routes>
-        </BrowserRouter>
+        <AppRoutes></AppRoutes>
       </BookingProvider>
     </SearchProvider>
   );
